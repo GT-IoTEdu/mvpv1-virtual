668a2fcf372d47f9a0d0c4e05afa02b6bc9b613c
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2026-04-15 08:46:17 -0300

    Add files via upload

--- deploy_atacante-alvo.sh
+++ deploy_atacante-alvo.sh
@@ -5,21 +5,11 @@ docker rm -f servidor_alvo atacante 2>/dev/null
 
 # Criar container servidor_alvo (sem --mac-address pois será sobrescrito)
 docker run -d --name servidor_alvo --hostname servidor_alvo \
-    --network none \
+    --network host \
     --cap-add NET_ADMIN --cap-add NET_RAW \
     servidor_alvo:latest sleep infinity
 
 
-sudo ip link add veth-host-01 type veth peer name veth-cont-01
-
-# Definir MAC address na interface que irá para o container
-sudo ip link set veth-cont-01 address 92:d0:c6:0a:29:32
-
-# Configurar bridge
-sudo ip link set veth-host-01 master bridge-tap
-sudo ip link set veth-host-01 up
-CPID=$(docker inspect -f '{{.State.Pid}}' servidor_alvo)
-sudo ip link set veth-cont-01 netns "${CPID}"
 docker exec -d servidor_alvo ./entrypoint.sh
 
 
@@ -27,7 +17,7 @@ docker exec -d servidor_alvo ./entrypoint.sh
 
 sleep 10
 # Obter IP
-SERVER_ALVO=$(docker exec servidor_alvo ip -4 addr show eth0 2>/dev/null | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+SERVER_ALVO=$(ip a | grep -A3 -B2 'mtu 1500 qdisc fq_codel state UP' | grep 'inet' | grep -Eo '(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))' | sort | uniq | grep -v '255')
 
 
 echo "IP do servidor: $SERVER_ALVO"
