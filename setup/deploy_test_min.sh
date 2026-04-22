eb2d7f8c614e06b0912772fb877d2fc158888e3a
Angelo <72932783+Angelodiniz@users.noreply.github.com>
2026-04-22 19:21:40 -0300

    Update deploy_test_min.sh

--- setup/deploy_test_min.sh
+++ setup/deploy_test_min.sh
@@ -9,7 +9,7 @@ atacante:latest sleep infinity
 
 
 sudo ip link add veth-host-04 type veth peer name veth-cont-04
-sudo ip link set veth-cont-04 address 92:d0:c6:0a:29:33
+sudo ip link set veth-cont-04 address $s1
 sudo ip link set veth-host-04 master bridge-tap
 sudo ip link set veth-host-04 up
 C02PID=$(docker inspect -f '{{.State.Pid}}' test_min)
